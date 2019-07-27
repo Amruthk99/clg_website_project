fdf9fab06606f64a995346b8dec3f2b357397c56
Amruth <amruthk99@gmail.com>
2019-07-27 12:06:46 +0530

    added some html files

--- subpages/views.py
+++ subpages/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from .models import Content_with_1_image
-from .models import Photo
+from .models import Photo,CSE_Faculty,ECE_Faculty
 # Create your views here.
 
 def aboutIIITT(request):
-    content = SubPageContent.objects.all
+    content = Content_with_1_image.objects.all
     args = {'content':content,}
-    return render(request,'subpage.html',args)
+    return render(request,'profile.html',args)
 
 def detail(request,page_id):
     content = Content_with_1_image.objects.get(title = page_id)
@@ -18,3 +18,9 @@ def gallery(request):
     content = Photo.objects.all
     args = {'content':content,}
     return render(request,'subpage.html',args)
+
+def faculty(request):
+    cse_faculty = CSE_Faculty.objects.all
+    ece_faculty = ECE_Faculty.objects.all
+    args = {'cse_faculty':cse_faculty,'ece_faculty':ece_faculty,}
+    return render(request,'faculty.html',args)
